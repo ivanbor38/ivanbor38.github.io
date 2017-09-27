4dd91e85e3cfc4fb3a85ef3d85afdcf782aa978b
unknown <aerw@yandex.ru>
2017-09-27 08:04:08 +0300

    first commit

---
+++ hw1/hw1.js
@@ -0,0 +1,20 @@
+var quotes = [];
+
+function parse() {
+
+    var xmlhttp = new XMLHttpRequest();
+
+
+    xmlhttp.open("GET", "https://raw.githubusercontent.com/4skinSkywalker/Database-Quotes-JSON/master/quotes.json", true);
+    xmlhttp.send();
+
+    quotes = JSON.parse(request.responseText);
+
+}
+
+
+window.onload = function() {
+    parse();
+    var rand = quotes[Math.floor(Math.random() * quotes.length)];
+    document.getElementById('one').innerHTML = '"' + rand.quoteText + '"' + " - " + rand.quoteAuthor
+    }
