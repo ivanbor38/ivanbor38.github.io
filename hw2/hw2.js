d64588f89d853c97817e0bbfae58f1083657ade3
unknown <aerw@yandex.ru>
2017-10-14 17:40:48 +0300

    Added checkbox filtering

--- hw2/hw2.js
+++ hw2/hw2.js
@@ -7,113 +7,177 @@
   
       
 d3.json("countries_2012.json", function(error, data){
-  var desired_cols = ['name', 'continent', 'gdp', 'life_expectancy', 'population', 'year']
-  //var columns = Object.keys(data[0]);
-  var columns = desired_cols;
-  var table = d3.select("body").append("table"),
-    thead = table.append("thead")
-                 .attr("class", "thead");
-    tbody = table.append("tbody");
+  
+    var desired_cols = ['name', 'continent', 'gdp', 'life_expectancy', 'population', 'year']
+  
+    var columns = desired_cols;
+  
+    var table = d3.select("body").append("table"),
+          thead = table.append("thead")
+                       .attr("class", "thead");
+          tbody = table.append("tbody");
 
-  table.append("caption")
-    .html("World Countries Ranking");
-
-  thead.append("tr").selectAll("th")
-    .data(columns)
-  .enter()
-    .append("th")
-    .text(function(d) { return d; })
-    .on("click", function(header, i) {
+        table.append("caption")
+          .html("World Countries Ranking");
+  
+  
+  
+  
+  d3.selectAll(".myCheckbox").on("change",update);
+    
+    
+    
+    
+    
+    function update(){
+        
+      d3.select('thead').remove();
+      d3.select('tbody').remove();
+      
+      
+        thead = table.append("thead")
+                       .attr("class", "thead");
+      
+        tbody = table.append("tbody");
         
         
-        
-        
-        var element = document.getElementsByTagName("img"), index;
-
-        for (index = element.length - 1; index >= 0; index--) {
-            element[index].parentNode.removeChild(element[index]);
+      var choices = [];
+      d3.selectAll(".myCheckbox").each(function(d){
+        cb = d3.select(this);
+        if(cb.property("checked")){
+          choices.push(cb.property("value"));
         }
-        
-        
-       var pics = [{src:"001.png"}];
+      });
+      
+    
+    
+    if (choices.length == 0) {
+        choices = ['Americas', 'Africa', 'Asia', 'Europe', 'Oceania'];
+    }
+    newData = data.filter(function(d,i){return choices.includes(d['continent']);});
+    
 
-    thead.selectAll('th')
-        //.filter(function(d, i) { return d == header; })
-        .data(pics)
-        .append('img')
-        .attr('src', function(d) {
-            return d.src;
+    var rows = tbody.selectAll("tr.row")
+    .data(newData);
+    
+    
+    rows.enter()
+    .append("tr").attr("class", "row");
+    rows.exit().remove();
+    
+    
+      var cells = rows.selectAll("td")
+        .data(function(row) {
+            return d3.range(columns.length).map(function(column, i) {
+                if (column == 2) {
+                    return d3.format(".3s")(row[columns[i]]);
+                }
+                if (column == 3) {
+                    return d3.format(".1f")(row[columns[i]]);
+                }
+                if (column == 4) {
+                    return d3.format(",")(row[columns[i]]);
+                }
+                
+                return row[columns[i]];
+            });
         });
+        cells.enter()
+        .append("td")
+        .text(function(d) { return d; });    
+        
+        cells.exit().remove();
+        
+        
+    thead.append("tr").selectAll("th")
+    .data(columns).enter().append("th").text(function(d) { return d; }).on("click", function(header, i) {
         
         
         
         
-        if (header == curr_header) {
-            cnt++;
+        d3.select("img").remove();
             
-            if (cnt % 2 == 1) {
+        
+            
+            if (header == curr_header) {
+                cnt++;
+                
+                if (cnt % 2 == 1) {
+                    d3.select(this).append('img').attr('src', '002.png');
+                    d3.select(this).style("cursor", "n-resize");
+                    tbody.selectAll("tr").sort(function(a, b) {
+                        if (header == 'continent') {
+                            
+                            return d3.ascending(a[header] + a['name'], b[header] + b['name']);
+                        }
+                        return d3.ascending(a[header], b[header]);
+                    });
+                    
+                }
+                else {
+                    d3.select(this).append('img').attr('src', '001.png');               
+                
+                    tbody.selectAll("tr").sort(function(a, b) {
+                        if (header == 'continent') {
+                                return d3.descending(a[header] + a['name'], b[header] + b['name']);
+                            }
+                        
+                      return d3.descending(a[header], b[header]); 
+                        });
+                }
+            }
+            
+            else {
+                
+                d3.select(this).append('img').attr('src', '001.png');
+                cnt = 0;
+                curr_header = header;
                 tbody.selectAll("tr").sort(function(a, b) {
                     if (header == 'continent') {
-                        return d3.ascending(a[header] + a['name'], b[header] + b['name']);
-                    }
-                    return d3.ascending(a[header], b[header]);
-                });
-                return;
+                                return d3.descending(a[header] + a['name'], b[header] + b['name']);
+                            }
+                      return d3.descending(a[header], b[header]);
+                        });
             }
-            
-            tbody.selectAll("tr").sort(function(a, b) {
-                if (header == 'continent') {
-                        return d3.descending(a[header] + a['name'], b[header] + b['name']);
-                    }
-                
-              return d3.descending(a[header], b[header]); 
-                });
-            return;
-        }
-        
-        
-        cnt = 0;
-        curr_header = header;
-        return tbody.selectAll("tr").sort(function(a, b) {
-            if (header == 'continent') {
-                        return d3.descending(a[header] + a['name'], b[header] + b['name']);
-                    }
-              return d3.descending(a[header], b[header]);
-                });
-       
-    });
-    
-    
-
-  var rows = tbody.selectAll("tr.row")
-    .data(data)
-    .enter()
-    .append("tr").attr("class", "row");
-
-  var cells = rows.selectAll("td")
-    .data(function(row) {
-        return d3.range(columns.length).map(function(column, i) {
-            if (column == 2) {
-                return d3.format(".3s")(row[columns[i]]);
-            }
-            if (column == 3) {
-                return d3.format(".1f")(row[columns[i]]);
-            }
-            if (column == 4) {
-                return d3.format(",")(row[columns[i]]);
-            }
-            
-            return row[columns[i]];
+           
         });
-    })
-    .enter()
-    .append("td")
-    .text(function(d) { return d; });
+        
+        //theader.exit().remove();
+        }
+  
+    update();
+  //-------------------------------------------------------------------------------------------------
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
     
     
-    
-    
-    
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+
+
+
+
+
+
+
+
+
+  
+  //rows.exit().remove();
+
+    //console.log(rows);
+    
 }
 )
+
+
+
+    
