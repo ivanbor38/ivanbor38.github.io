8148febdd496a4a94963ebe8ebf8d1f559116906
unknown <aerw@yandex.ru>
2017-10-15 08:58:30 +0300

    1-4 parts completed

--- hw2/hw2.js
+++ hw2/hw2.js
@@ -6,7 +6,26 @@
   
   
       
-d3.json("countries_2012.json", function(error, data){
+d3.json("countries_1995_2012.json", function(error, data){
+    
+    tmp_data = [];
+    
+    for (i=0; i< data.length; i++) {
+        for (j=0; j<data[i]['years'].length; j++) {
+            obj = {
+                'name': data[i]['name'],
+                'continent': data[i]['continent'],
+                'gdp': data[i]['years'][j]['gdp'],
+                'life_expectancy': data[i]['years'][j]['life_expectancy'],
+                'population': data[i]['years'][j]['population'],
+                'year': data[i]['years'][j]['year']
+            }
+            tmp_data.push(obj);
+        }
+    }
+    
+    data = tmp_data;
+    
   
     var desired_cols = ['name', 'continent', 'gdp', 'life_expectancy', 'population', 'year']
   
@@ -21,18 +40,30 @@ d3.json("countries_2012.json", function(error, data){
           .html("World Countries Ranking");
   
   
+    //console.log(data[0]);
   
-  
-  d3.selectAll(".myCheckbox").on("change",update);
+    d3.selectAll(".myCheckbox").on("change",update);
+    d3.selectAll(".aggregation").on("change",update);
+    d3.select('.range').on("change", update);
+    var agg = 'None';
     
+    function changeAgg() {
+    
+        d3.selectAll(".aggregation").each(function (d) {
+            rb = d3.select(this);
+            if (rb.property('checked')) {
+                agg = rb.property("value");
+                }
+        });
+    }
     
     
     
     
     function update(){
         
-      d3.select('thead').remove();
-      d3.select('tbody').remove();
+        d3.select('thead').remove();
+        d3.select('tbody').remove();
       
       
         thead = table.append("thead")
@@ -41,56 +72,118 @@ d3.json("countries_2012.json", function(error, data){
         tbody = table.append("tbody");
         
         
-      var choices = [];
-      d3.selectAll(".myCheckbox").each(function(d){
-        cb = d3.select(this);
-        if(cb.property("checked")){
-          choices.push(cb.property("value"));
-        }
-      });
+        var choices = [];
+        d3.selectAll(".myCheckbox").each(function(d){
+          cb = d3.select(this);
+          if(cb.property("checked")){
+            choices.push(cb.property("value"));
+              }
+          });
       
     
+        var curr_year = d3.select('.range').property('value');
+        if (choices.length == 0) {
+            choices = ['Americas', 'Africa', 'Asia', 'Europe', 'Oceania'];
+        }
+        
+        newData = data.filter(function(d,i){
+            return choices.includes(d['continent']) && d['year'] == curr_year;});
     
-    if (choices.length == 0) {
-        choices = ['Americas', 'Africa', 'Asia', 'Europe', 'Oceania'];
-    }
-    newData = data.filter(function(d,i){return choices.includes(d['continent']);});
+        
+        //console.log(curr_year);
+        
+        
+        
     
+        changeAgg();
+        //console.log(newData);
+    
+        if (agg == "by Countries") {
+            newData = d3.nest()
+                .key(function (d) { return d['continent']; })
+                .rollup(function (rws) {
+                    var aggTable = {
+                        gdp: 0,
+                        continent: rws[0]['continent'],
+                        name: rws[0]['continent'],
+                        life_expectancy: 0,
+                        population: 0,
+                        year: rws[0]['year']
+                    };
+                    
+                    var numberOfCountries = 0;
+                    rws.forEach(function (rw) {
+                        numberOfCountries++;
+                        
+                        aggTable['gdp'] += rw['gdp'];
+                        aggTable['life_expectancy'] += rw['life_expectancy'];
+                        aggTable['population'] += rw['population'];
+                        
+                    });
+                    
+                    aggTable['life_expectancy'] /= numberOfCountries;
+                    //console.log(aggTable);
+                    return aggTable;
+                }).entries(newData);
 
-    var rows = tbody.selectAll("tr.row")
-    .data(newData);
+            
+            newData = newData.map(function (dict) {return dict.value;});
+            
+            //console.log(newData);
+        }    
     
     
-    rows.enter()
-    .append("tr").attr("class", "row");
-    rows.exit().remove();
     
     
-      var cells = rows.selectAll("td")
-        .data(function(row) {
-            return d3.range(columns.length).map(function(column, i) {
-                if (column == 2) {
-                    return d3.format(".3s")(row[columns[i]]);
-                }
-                if (column == 3) {
-                    return d3.format(".1f")(row[columns[i]]);
-                }
-                if (column == 4) {
-                    return d3.format(",")(row[columns[i]]);
-                }
-                
-                return row[columns[i]];
+    
+    
+        //var rows = tbody.selectAll("tr.row")
+        //.data(newData);
+        //
+        //
+        //        
+        //
+        //rows.enter()
+        //.append("tr").attr("class", "row");
+        //
+        //
+        //rows.exit().remove();
+        
+        
+        var rows = tbody.selectAll("tr.row")
+                .data(newData)
+                .enter()
+                .append("tr").attr("class", "row");
+        
+        
+        //console.log(rows);
+        
+          var cells = rows.selectAll("td")
+            .data(function(row) {
+                //console.log(row);
+                return d3.range(columns.length).map(function(column, i) {
+                    if (column == 2) {
+                        return d3.format(".3s")(row[columns[i]]);
+                    }
+                    if (column == 3) {
+                        return d3.format(".1f")(row[columns[i]]);
+                    }
+                    if (column == 4) {
+                        return d3.format(",")(row[columns[i]]);
+                    }
+                    
+                    return row[columns[i]];
+                });
             });
-        });
-        cells.enter()
-        .append("td")
-        .text(function(d) { return d; });    
-        
-        cells.exit().remove();
-        
-        
-    thead.append("tr").selectAll("th")
-    .data(columns).enter().append("th").text(function(d) { return d; }).on("click", function(header, i) {
+            cells.enter()
+            .append("td")
+            .text(function(d) { return d; });    
+            
+            cells.exit().remove();
+            
+            
+        thead.append("tr").selectAll("th")
+        .data(columns).enter().append("th").text(function(d) { return d; }).on("click", function(header, i) {
         
         
         
@@ -141,6 +234,13 @@ d3.json("countries_2012.json", function(error, data){
             }
            
         });
+        
+        
+        
+        
+        
+        
+        
         
         //theader.exit().remove();
         }
